e257dbb4624b60e9cea1acb78d064a577f2a9417
chumaidi <chumaidihxc@gmail.com>
2019-06-25 14:59:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const rectangleWidth = 100
+const rectangleLength = 20
+
+const rectangleArea = rectangleWidth * rectangleLength
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+
+// The formula of trapezoid area is
+// Area = b1 + b2 / 2
+
+const Lengthb1 = 100
+const Lengthb2 = 120
+
+const trapezoidArea = Lengthb1 + Lengthb2 / 2
+
+console.log(`${trapezoidArea} = ${Lengthb1} + ${Lengthb2} / 2
